3ec24ff0fbd48cf8e307c4d7810b29d020e4592d
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2020-03-22 18:30:39 -0500

    Adding new implementation of makeNewPuzzle()
    This new method replaces shuffle() which didn't always make a valid, solvable puzzle.
    
    Also fixing a potential problem in isPuzzleSolved(). If NUM_ROWS and NUM_COLS are different it would have constructed the wrong className when checking that the tiles were in order.

--- slidepuzzle/main.js
+++ slidepuzzle/main.js
@@ -1,7 +1,13 @@
 const NUM_ROWS = 3;
 const NUM_COLS = 3;
-const EMPTY_TILE_CLASSNAME = "tile" + (NUM_ROWS * NUM_COLS - 1);
+const EMPTY_TILE_NUMBER = NUM_ROWS * NUM_COLS - 1;
+const EMPTY_TILE_CLASSNAME = "tile" + EMPTY_TILE_NUMBER;
 const FINAL_TILE_CLASSNAME = "tile" + (NUM_ROWS * NUM_COLS);
+
+function setCompletedText(text) {
+    var element = document.getElementById("puzzleComplete");
+    element.innerText = text;
+}
 
 // Returns the document element for the specified cell
 function getCellElement(row, column) {
@@ -15,22 +21,16 @@ function isTileCellEmpty(cell) {
 }
 
 // Parameters are document elements as returned by getCellElement()
-function swapTileCells(cell1, cell2) {
+function swapTiles(cell1, cell2) {
     var temp = cell1.className;
     cell1.className = cell2.className;
     cell2.className = temp;
 }
 
-function swapTiles(row1, col1, row2, col2) {
-    var cell1 = getCellElement(row1, col1);
-    var cell2 = getCellElement(row2, col2);
-    swapTileCells(cell1, cell2);
-}
-
 function isPuzzleSolved() {
     for (var row = 0; row < NUM_ROWS; row++) {
         for (var col = 0; col < NUM_COLS; col++) {
-            if (getCellElement(row, col).className != "tile" + (row * NUM_ROWS + col)) {
+            if (getCellElement(row, col).className != "tile" + (row * NUM_COLS + col)) {
                 return false;
             }
         }
@@ -44,24 +44,52 @@ function checkAndMarkPuzzleSolved() {
     }
     // Replace the empty tile with the "final" tile
     getCellElement(NUM_ROWS - 1, NUM_COLS - 1).className = FINAL_TILE_CLASSNAME;
-    var element = document.getElementById("puzzleComplete");
-    element.innerText = "Complete!";
-    // Hide the new game button, when the puzzle is in this state shuffle() makes it unsolvable
-    element = document.getElementById("newGameButton");
-    element.style.display = "none";
+    setCompletedText("Complete!");
 }
 
-function shuffle() {
-    // Use nested loops to access each cell of the grid
-    for (var row = 0; row < NUM_ROWS; row++) { // For each row of the grid
-        for (var column = 0; column < NUM_COLS; column++) { // For each column in this row
+function makeNewPuzzle() {
+    // Starting condition of the puzzle, cells 0..8 contain tiles 0..8
+    // Cell index: upper left = 0, lower right = 8
+    var tileNums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    // Which cell contains the empty tile initially
+    var emptyCell = 8;
+    // Cells that are adjacent to any cell, i.e. cells 1 & 3 are adjacent to cell 0
+    // Adjacent cells can be swapped with the empty cell to make a valid move
+    var adjacentCells =
+        [
+            [1, 3], // 0
+            [0, 4, 2], // 1
+            [1, 5], // 2
+            [0, 4, 6], // 3
+            [1, 3, 5, 7], // 4
+            [2, 4, 8], // 5
+            [3, 7], // 6
+            [6, 4, 8], // 7
+            [5, 7] // 8
+        ];
 
-            var row2 = Math.floor(Math.random() * NUM_ROWS); // Pick a random row from 0 to NUM_ROWS
-            var column2 = Math.floor(Math.random() * NUM_COLS); // Pick a random column from 0 to NUM_COLS
+    for (var t = 0; t < 300; t++) {
+        // First pick a cell to move the empty cell to
+        var adjacentRow = adjacentCells[emptyCell];
+        var random = Math.floor(Math.random() * adjacentRow.length);
+        var cellToMove = adjacentRow[random];
 
-            swapTiles(row, column, row2, column2); // Swap the look & feel of both cells
+        // Move the tile # in cellToMove to emptyCell
+        tileNums[emptyCell] = tileNums[cellToMove];
+        // Make cellToMove the empty tile #
+        tileNums[cellToMove] = EMPTY_TILE_NUMBER;
+        emptyCell = cellToMove;
+    }
+
+    // Set all of the cells to the tile #s we just randomly moved
+    for (var row = 0; row < NUM_ROWS; row++) {
+        for (var col = 0; col < NUM_COLS; col++) {
+            getCellElement(row, col).className = "tile" + tileNums[row * NUM_COLS + col];
         }
     }
+
+    // Puzzle is no longer complete
+    setCompletedText("");
 }
 
 function clickTile(row, column) {
@@ -75,7 +103,7 @@ function clickTile(row, column) {
     if (column < NUM_COLS - 1) {
         var cell2 = getCellElement(row, column + 1);
         if (isTileCellEmpty(cell2)) {
-            swapTileCells(cell1, cell2);
+            swapTiles(cell1, cell2);
             checkAndMarkPuzzleSolved();
             return;
         }
@@ -84,7 +112,7 @@ function clickTile(row, column) {
     if (column > 0) {
         var cell2 = getCellElement(row, column - 1);
         if (isTileCellEmpty(cell2)) {
-            swapTileCells(cell1, cell2);
+            swapTiles(cell1, cell2);
             checkAndMarkPuzzleSolved();
             return;
         }
@@ -93,7 +121,7 @@ function clickTile(row, column) {
     if (row > 0) {
         var cell2 = getCellElement(row - 1, column);
         if (isTileCellEmpty(cell2)) {
-            swapTileCells(cell1, cell2);
+            swapTiles(cell1, cell2);
             checkAndMarkPuzzleSolved();
             return;
         }
@@ -102,7 +130,7 @@ function clickTile(row, column) {
     if (row < NUM_ROWS - 1) {
         var cell2 = getCellElement(row + 1, column);
         if (isTileCellEmpty(cell2)) {
-            swapTileCells(cell1, cell2);
+            swapTiles(cell1, cell2);
             checkAndMarkPuzzleSolved();
             return;
         }
