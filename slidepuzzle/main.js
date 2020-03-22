141cc7bfc9fe3466e83d9a5c89ebdb8fae118ce5
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2020-03-22 10:10:59 -0500

    Adding constants for puzzle rows & columns
    Note that constants in Javascript were introduced in ECMAScript 2015 and not supported in older browsers. See the following link for a description and Browser Support.
    
    https://www.w3schools.com/js/js_const.asp

--- slidepuzzle/main.js
+++ slidepuzzle/main.js
@@ -1,3 +1,6 @@
+const NUM_ROWS = 3;
+const NUM_COLS = 3;
+
 function swapTiles(cell1, cell2) {
     var temp = document.getElementById(cell1).className;
     document.getElementById(cell1).className = document.getElementById(cell2).className;
@@ -5,12 +8,12 @@ function swapTiles(cell1, cell2) {
 }
 
 function shuffle() {
-    // Use nested loops to access each cell of the 3x3 grid
-    for (var row = 0; row < 3; row++) { // For each row of the 3x3 grid
-        for (var column = 0; column < 3; column++) { // For each column in this row
+    // Use nested loops to access each cell of the grid
+    for (var row = 0; row < NUM_ROWS; row++) { // For each row of the grid
+        for (var column = 0; column < NUM_COLS; column++) { // For each column in this row
 
-            var row2 = Math.floor(Math.random() * 3); // Pick a random row from 0 to 2
-            var column2 = Math.floor(Math.random() * 3); // Pick a random column from 0 to 2
+            var row2 = Math.floor(Math.random() * NUM_ROWS); // Pick a random row from 0 to NUM_ROWS
+            var column2 = Math.floor(Math.random() * NUM_COLS); // Pick a random column from 0 to NUM_COLS
 
             swapTiles("cell" + row + column, "cell" + row2 + column2); // Swap the look & feel of both cells
         }
@@ -22,7 +25,7 @@ function clickTile(row, column) {
     var tile = cell.className;
     if (tile != "tile8") {
         // Checking if white tile on the right
-        if (column < 2) {
+        if (column < NUM_COLS - 1) {
             if (document.getElementById("cell" + row + (column + 1)).className == "tile8") {
                 swapTiles("cell" + row + column, "cell" + row + (column + 1));
                 return;
@@ -43,7 +46,7 @@ function clickTile(row, column) {
             }
         }
         // Checking if white tile is below
-        if (row < 2) {
+        if (row < NUM_ROWS - 1) {
             if (document.getElementById("cell" + (row + 1) + column).className == "tile8") {
                 swapTiles("cell" + row + column, "cell" + (row + 1) + column);
                 return;
